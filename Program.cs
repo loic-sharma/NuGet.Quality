6fcb1d2e47568a721d1d6fdf62b161c22ab2ad8a
Loic Sharma <sharma.loic@gmail.com>
2020-02-23 22:16:01 -0800

    Work

--- Program.cs
+++ Program.cs
@@ -96,79 +96,212 @@ namespace NuGet.Quality
         // https://github.com/dotnet/runtime/blob/4f9ae42d861fcb4be2fcd5d3d55d5f227d30e723/src/libraries/System.Reflection.Metadata/src/System/Reflection/Metadata/PortablePdb/PortablePdbVersions.cs#L41
         private const ushort PortableCodeViewVersionMagic = 0x504d;
 
+        // https://github.com/dotnet/roslyn/blob/b3cbe7abce7633e45d7dd468bde96bfe24ccde47/src/Dependencies/CodeAnalysis.Debugging/PortableCustomDebugInfoKinds.cs#L18
+        private static readonly Guid SourceLinkMagic = new Guid("CC110556-A091-4D38-9FEC-25AB9A351A6A");
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
         public async Task ValidateAsync(ValidationContext ctx, CancellationToken cancellationToken)
         {
-            var httpClient = new HttpClient();
             var items = new HashSet<string>(ctx.PackageReader.GetLibItems().SelectMany(g => g.Items));
 
             foreach (var item in items)
             {
                 if (Path.GetExtension(item).ToLowerInvariant() != ".dll") continue;
 
-                // Check if the package has a corresponding symbol file for the assembly.
-                var symbolPath = Path.GetDirectoryName(item) + "/" + Path.GetFileNameWithoutExtension(item) + ".pdb";
-                if (items.Contains(symbolPath))
+                await ValidatePackageAssembly(ctx, item, cancellationToken);
+            }
+        }
+
+        private async Task ValidatePackageAssembly(ValidationContext ctx, string item, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(item);
+
+            using var fileStream = new MemoryStream();
+            using (var rawStream = ctx.PackageReader.GetStream(item))
+            {
+                await rawStream.CopyToAsync(fileStream, cancellationToken);
+                fileStream.Position = 0;
+            }
+
+            using var peReader = new PEReader(fileStream);
+
+            // Check if the package has a corresponding symbol file for the assembly.
+            var symbolPath = Path.GetDirectoryName(item) + "/" + Path.GetFileNameWithoutExtension(item) + ".pdb";
+            if (ctx.PackageReader.GetFiles().Any(path => path == symbolPath))
+            {
+                // TODO: Check that the PDB and DLL match.
+                // See: https://github.com/NuGet/NuGet.Jobs/blob/master/src/Validation.Symbols/SymbolsValidatorService.cs#L190-L249
+                var pdbStream = ctx.PackageReader.GetStream(symbolPath);
+                if (!HasSourceLinkDebugInformation(pdbStream))
                 {
-                    // TODO: Check that the PDB and DLL match.
-                    // See: https://github.com/NuGet/NuGet.Jobs/blob/master/src/Validation.Symbols/SymbolsValidatorService.cs#L190-L249
-                    //continue;
-                }
-
-                // TODO: Check if the symbols are embedded in the assembly.
-                using var fileStream = new MemoryStream();
-                using (var rawStream = ctx.PackageReader.GetStream(item))
-                {
-                    await rawStream.CopyToAsync(fileStream, cancellationToken);
-                    fileStream.Position = 0;
-                }
-
-                using var reader = new PEReader(fileStream);
-
-                var debugEntries = reader.ReadDebugDirectory().ToList();
-
-                Console.WriteLine(item);
-
-                // Phase 1 - check if the DLL has embedded symbols.
-                // TODO: Is this right??
-                if (reader.ReadDebugDirectory().Any(e => e.Type == DebugDirectoryEntryType.EmbeddedPortablePdb))
-                {
-                    //continue;
-                }
-
-                // TODO: Check symbol servers.
-                // See: https://github.com/dotnet/symstore/blob/16544a43620dfc9d06a907fc1c8970b7f3b671cb/src/Microsoft.FileFormats/PE/PEFile.cs#L172-L184
-                foreach (var entry in reader.ReadDebugDirectory())
-                {
-                    if (entry.Type != DebugDirectoryEntryType.CodeView) continue;
-
-                    var data = reader.ReadCodeViewDebugDirectoryData(entry);
-                    var isPortable = entry.MinorVersion == PortableCodeViewVersionMagic;
-                    var signature = data.Guid;
-                    var age = data.Age;
-                    var file = Uri.EscapeDataString(Path.GetFileName(data.Path.Replace('\\', '/')).ToLowerInvariant());
-
-                    // Portable PDBs, see: https://github.com/dotnet/symstore/blob/83032682c049a2b879790c615c27fbc785b254eb/src/Microsoft.SymbolStore/KeyGenerators/PortablePDBFileKeyGenerator.cs#L84
-                    // Windows PDBs, see: https://github.com/dotnet/symstore/blob/83032682c049a2b879790c615c27fbc785b254eb/src/Microsoft.SymbolStore/KeyGenerators/PDBFileKeyGenerator.cs#L52
-                    var symbolId = isPortable
-                        ? signature.ToString("N") + "FFFFFFFF"
-                        : string.Format("{0}{1:x}", signature.ToString("N"), age);
-
-                    Console.WriteLine();
-                    Console.WriteLine(file);
-                    Console.WriteLine(symbolId);
-
-                    var uri = new Uri(new Uri("https://symbols.nuget.org/download/symbols/"), $"{file}/{symbolId}/{file}");
-
-                    // TODO
-                    using var request = new HttpRequestMessage();
-                    request.Method = HttpMethod.Get;
-                    request.RequestUri = uri;
-                    request.Headers.Add("SymbolChecksum", string.Join(";", new[] { "test", "hello" } ));
-
-                    var response = await httpClient.SendAsync(request, cancellationToken);
-                    Console.WriteLine(response.StatusCode + " " + uri);
+                    ctx.Messages.Add("The NuGet package does not have SourceLink");
+                    return;
                 }
             }
+
+            // Check if the assembly has embedded symbols.
+            if (peReader.ReadDebugDirectory().Any(e => e.Type == DebugDirectoryEntryType.EmbeddedPortablePdb))
+            {
+                var embeddedEntry = peReader.ReadDebugDirectory().Single(e => e.Type == DebugDirectoryEntryType.EmbeddedPortablePdb);
+
+                using (var embeddedMetadataProvider = peReader.ReadEmbeddedPortablePdbDebugDirectoryData(embeddedEntry))
+                {
+                    var pdbReader = embeddedMetadataProvider.GetMetadataReader();
+
+                    if (!HasSourceLinkDebugInformation(pdbReader))
+                    {
+                        ctx.Messages.Add("The NuGet package does not have SourceLink");
+                        return;
+                    }
+                }
+            }
+
+            // The assembly does not have symbols within the package. Try to load the symbols from a symbol server.
+            // See: https://github.com/dotnet/symstore/blob/16544a43620dfc9d06a907fc1c8970b7f3b671cb/src/Microsoft.FileFormats/PE/PEFile.cs#L172-L184
+            var symbolKeys = GetSymbolKeys(peReader);
+            var portableSymbolKeys = symbolKeys.Where(k => k.IsPortablePdb).ToList();
+            var windowsSymbolKeys = symbolKeys.Where(k => !k.IsPortablePdb).ToList();
+
+            if (portableSymbolKeys.Any())
+            {
+                using var pdbStream = await GetSymbolsAsync(portableSymbolKeys, cancellationToken);
+                if (pdbStream != null)
+                {
+                    if (!HasSourceLinkDebugInformation(pdbStream))
+                    {
+                        ctx.Messages.Add("The NuGet package does not have SourceLink");
+                    }
+
+                    return;
+                }
+            }
+
+            if (windowsSymbolKeys.Any())
+            {
+                using var pdbStream = await GetSymbolsAsync(windowsSymbolKeys, cancellationToken);
+                if (pdbStream != null)
+                {
+                    ctx.Messages.Add("The NuGet package does not have SourceLink");
+                    return;
+                }
+            }
+
+            ctx.Messages.Add("NuGet package does not have symbols");
+        }
+
+        private IReadOnlyList<SymbolKey> GetSymbolKeys(PEReader peReader)
+        {
+            var result = new List<SymbolKey>();
+            var checksums = new List<string>();
+
+            foreach (var entry in peReader.ReadDebugDirectory())
+            {
+                if (entry.Type != DebugDirectoryEntryType.PdbChecksum) continue;
+
+                var data = peReader.ReadPdbChecksumDebugDirectoryData(entry);
+                var algorithm = data.AlgorithmName;
+                var checksum = data.Checksum.Select(b => b.ToString("x2"));
+
+                checksums.Add($"{algorithm}:{checksum}");
+            }
+
+            foreach (var entry in  peReader.ReadDebugDirectory())
+            {
+                if (entry.Type != DebugDirectoryEntryType.CodeView) continue;
+
+                var data = peReader.ReadCodeViewDebugDirectoryData(entry);
+                var isPortable = entry.MinorVersion == PortableCodeViewVersionMagic;
+
+                var signature = data.Guid;
+                var age = data.Age;
+                var file = Uri.EscapeDataString(Path.GetFileName(data.Path.Replace('\\', '/')).ToLowerInvariant());
+
+                // Portable PDBs, see: https://github.com/dotnet/symstore/blob/83032682c049a2b879790c615c27fbc785b254eb/src/Microsoft.SymbolStore/KeyGenerators/PortablePDBFileKeyGenerator.cs#L84
+                // Windows PDBs, see: https://github.com/dotnet/symstore/blob/83032682c049a2b879790c615c27fbc785b254eb/src/Microsoft.SymbolStore/KeyGenerators/PDBFileKeyGenerator.cs#L52
+                var symbolId = isPortable
+                    ? signature.ToString("N") + "FFFFFFFF"
+                    : string.Format("{0}{1:x}", signature.ToString("N"), age);
+
+                result.Add(new SymbolKey
+                {
+                    IsPortablePdb = isPortable,
+                    Checksums = checksums,
+                    Key = $"{file}/{symbolId}/{file}",
+                });
+            }
+
+            return result;
+        }
+
+        private async Task<Stream> GetSymbolsAsync(
+            IReadOnlyList<SymbolKey> symbolKeys,
+            CancellationToken cancellationToken)
+        {
+            foreach (var symbolKey in symbolKeys)
+            {
+                var uri = new Uri(new Uri("https://symbols.nuget.org/download/symbols/"), symbolKey.Key);
+
+                using var request = new HttpRequestMessage();
+                request.Method = HttpMethod.Get;
+                request.RequestUri = uri;
+
+                if (symbolKey.Checksums.Any())
+                {
+                    request.Headers.Add("SymbolChecksum", string.Join(";", symbolKey.Checksums));
+                }
+
+                using (var response = await _httpClient.SendAsync(request, cancellationToken))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        continue;
+                    }
+
+                    var pdbStream = new MemoryStream();
+                    await response.Content.CopyToAsync(pdbStream);
+                    pdbStream.Position = 0;
+
+                    return pdbStream;
+                }
+            }
+
+            return null;
+        }
+
+        private bool HasSourceLinkDebugInformation(Stream pdbStream)
+        {
+            using var pdbReaderProvider = MetadataReaderProvider.FromPortablePdbStream(pdbStream);
+            var pdbReader = pdbReaderProvider.GetMetadataReader();
+
+            return HasSourceLinkDebugInformation(pdbReader);
+        }
+
+        private bool HasSourceLinkDebugInformation(MetadataReader pdbReader)
+        {
+            foreach (var customDebugInfoHandle in pdbReader.CustomDebugInformation)
+            {
+                var customDebugInfo = pdbReader.GetCustomDebugInformation(customDebugInfoHandle);
+                if (pdbReader.GetGuid(customDebugInfo.Kind) != SourceLinkMagic)
+                {
+                    continue;
+                }
+
+                var sourceLinkContent = pdbReader.GetBlobBytes(customDebugInfo.Value);
+                var sourceLinkText = System.Text.Encoding.UTF8.GetString(sourceLinkContent);
+
+                Console.WriteLine("Sourcelink: " + sourceLinkText);
+                return true;
+            }
+
+            return false;
+        }
+
+        private class SymbolKey
+        {
+            public bool IsPortablePdb { get; set; }
+            public IReadOnlyList<string> Checksums { get; set; }
+            public string Key { get; set; }
         }
     }
 
